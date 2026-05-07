97887e9b661de2d0365cd623761bd95ad645e5b0
Naite-Drake203 <162971506+Naite-Drake203@users.noreply.github.com>
2026-05-07 12:42:37 +0300

    Update data.py

--- helps/data.py
+++ helps/data.py
@@ -23,8 +23,8 @@ class Users:
 class Urls:
     DZEN_URL = "https://dzen.ru/?yredirect=true"
     QA_SCOOTER_URL = "https://qa-scooter.praktikum-services.ru/"
-    QA_SCOOTER_ORDER_URL = "https://qa-scooter.praktikum-services.ru/order"
-    QA_SCOOTER_STATUS_URL = "https://qa-scooter.praktikum-services.ru/track"
+    QA_SCOOTER_ORDER_URL = QA_SCOOTER_URL + "order"
+    QA_SCOOTER_STATUS_URL = QA_SCOOTER_URL + "track"
 
 
 class Questions:
